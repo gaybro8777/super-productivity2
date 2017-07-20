a4a338ca47c70033299920cec320c5797460bc63
Johannes Millan <johannes.millan@gmail.com>
2017-07-20 15:45:39 +0200

    feat: add super simple todo interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import 'hammerjs';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
+import { todoReducer } from './reducers/todo.reducer';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdToolbarModule} from '@angular/material';
@@ -21,10 +23,11 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MdToolbarModule,
     RouterModule.forRoot(routes, {useHash: true}),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({todoReducer}),
     StoreDevtoolsModule.instrument({
       maxAge: 25 //  Retains last 25 states
     }),
---
+++ src/app/reducers/todo.reducer.ts
@@ -0,0 +1,31 @@
+// import { Action } from '@ngrx/store';
+
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const TOGGLE_DONE = 'TOGGLE_DONE';
+
+
+// export function todoReducer(state = [], action: Action) {
+export function todoReducer(state = [], action: any) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [action.payload, ...state];
+    case DELETE_TODO:
+      return state.filter((item, index) => index !== action.payload);
+    case UPDATE_TODO:
+      return state.map((item, index) => {
+        return index === action.payload.index
+          ? Object.assign({}, item, { value: action.payload.newValue })
+          : item;
+      });
+    case TOGGLE_DONE:
+      return state.map((item, index) => {
+        return index === action.payload.index
+          ? Object.assign({}, item, { done: !action.payload.done })
+          : item;
+      });
+    default:
+      return state;
+  }
+}
