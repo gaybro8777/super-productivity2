2080af1b16d88e9d0eb4509f1f209fa9a054d9fb
Johannes Millan <johannes.millan@gmail.com>
2017-11-26 22:14:06 +0100

    refactor: also export filter functions by themselves for durations

--- src/app/duration/duration-from-string.pipe.ts
+++ src/app/duration/duration-from-string.pipe.ts
@@ -6,57 +6,58 @@ import * as moment from 'moment';
   name: 'durationFromString'
 })
 export class DurationFromStringPipe implements PipeTransform {
+  transform = durationFromString;
+}
 
-  transform(strValue: any, args?: any): any {
-    if (!strValue) {
-      return;
+export const durationFromString = (strValue: any, args?: any): any => {
+  if (!strValue) {
+    return;
+  }
+
+  let days;
+  let hours;
+  let minutes;
+  let seconds;
+  let momentVal;
+  let isValid;
+
+  const arrValue = strValue.split(' ');
+
+  arrValue.forEach((val) => {
+    if (val.length > 0) {
+      const lastChar = val.slice(-1);
+      const amount = parseInt(val.slice(0, val.length - 1), 10);
+
+      if (lastChar === 's') {
+        seconds = amount;
+      }
+      if (lastChar === 'm') {
+        minutes = amount;
+      }
+      if (lastChar === 'h') {
+        hours = amount;
+      }
+      if (lastChar === 'd') {
+        days = amount;
+      }
     }
+  });
+  isValid = seconds || minutes || hours || days || false;
 
-    let days;
-    let hours;
-    let minutes;
-    let seconds;
-    let momentVal;
-    let isValid;
-
-    const arrValue = strValue.split(' ');
-
-    arrValue.forEach((val) => {
-      if (val.length > 0) {
-        const lastChar = val.slice(-1);
-        const amount = parseInt(val.slice(0, val.length - 1), 10);
-
-        if (lastChar === 's') {
-          seconds = amount;
-        }
-        if (lastChar === 'm') {
-          minutes = amount;
-        }
-        if (lastChar === 'h') {
-          hours = amount;
-        }
-        if (lastChar === 'd') {
-          days = amount;
-        }
-      }
+  if (isValid) {
+    momentVal = moment.duration({
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
     });
-    isValid = seconds || minutes || hours || days || false;
 
-    if (isValid) {
-      momentVal = moment.duration({
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds,
-      });
-
-      if (momentVal.asSeconds() > 0) {
-        return momentVal;
-      } else {
-        return undefined;
-      }
+    if (momentVal.asSeconds() > 0) {
+      return momentVal;
     } else {
       return undefined;
     }
+  } else {
+    return undefined;
   }
-}
+};
